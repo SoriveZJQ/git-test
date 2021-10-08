52cc9faeda14f84f2ef63fdd0631ef0254090888
zizi <1792575431@qq.com>
2021-10-08 09:25:20 +0800

    first commit

---
+++ src/main/java/com/zizi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zizi.git;
+
+/**
+ * @author zizi
+ * @create 2021-10-08  09:21:32
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
